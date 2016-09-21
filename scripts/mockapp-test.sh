dab452b19626b35f1df84010acb19cb980eb25c3
binggo <binggozhan@163.com>
2016-09-21 16:27:29 +0800

    update scripts/mockapp-test.sh

--- scripts/mockapp-test.sh
+++ scripts/mockapp-test.sh
@@ -5,18 +5,19 @@
 
 HOST=http://localhost:9754
 
-curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d "{}" $HOST/toy/add 2> /dev/null
-exit
+#curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d "{}" $HOST/toy/add 2> /dev/null
+#exit
 
 # ----------- test /city ------------
 # insert
 #curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d "{\"name\": \"Shaoxing\", \"province\": \"ZheJiang\"}" $HOST/city 2> /dev/null
 #curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d "{\"name\": \"Nanjing\", \"province\": \"JiangSu\"}" $HOST/city 2> /dev/null
 #curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d "{\"name\": \"Hefei\", \"province\": \"AnHui\"}" $HOST/city 2> /dev/null
+curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d "{\"name\": \"Zhengzhou\", \"province\": \"HeNan\"}" $HOST/city 2> /dev/null
 
 # select
 #curl -X GET -H "Accept: application/json" $HOST/city/1 2> /dev/null
-#curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d "{\"pageNum\": 2, \"pageSize\": 3}" $HOST/city/list 2> /dev/null
+#curl -X GET -H "Accept: application/json" $HOST/city/list 2> /dev/null
 
 # update
 #curl -X PUT -H "Accept: application/json" -H "Content-type: application/json" $HOST/city/1 -d "{\"name\": \"Fuzhou\", \"province\": \"FuJian\"}" 2> /dev/null
@@ -32,5 +33,5 @@ exit
 #curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d "{\"name\": \"John\", \"phone\": \"120983\"}" $HOST/person 2> /dev/null
 
 # join select
-#curl -X GET  -H "Accept: application/json" -H "Content-type: application/json" -d "{\"pageNum\": 2, \"pageSize\": 3}" $HOST/person/list 2> /dev/null
+#curl -X GET  -H "Accept: application/json" $HOST/person/list 2> /dev/null
 
